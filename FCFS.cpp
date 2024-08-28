5ff44315ff10df4718d9d1d4c248f11576ed1c20
AMRITANSHU-ROY <123227611+AMRITANSHU-ROY@users.noreply.github.com>
2024-08-28 22:56:32 +0530

    Add files via upload

---
+++ FCFS.cpp
@@ -0,0 +1,59 @@
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+class Process {
+public:
+	int pid;
+	int priority;
+	int arrivalTime;
+	vector<string> bursts;
+	Process(int pid, int priority, int arrivalTime, const vector<string>& bursts)
+    	: pid(pid), priority(priority), arrivalTime(arrivalTime), bursts(bursts) {}
+};
+
+void fcfsScheduling(vector<Process>& processes) {
+	cout << "FCFS Scheduling:" << endl;
+
+	for (const Process& process : processes) {
+    	cout << "Process " << process.pid << ": Arrival Time=" << process.arrivalTime << " ";
+    	for (const string& burst : process.bursts) {
+        	cout << burst << " ";
+    	}
+    	cout << endl;
+	}
+}
+
+vector<Process> readInputFile(const string& filename) {
+	vector<Process> processes;
+	ifstream file(filename);
+	if (!file.is_open()) {
+    	cerr << "Error opening file: " << filename << endl;
+    	exit(EXIT_FAILURE);
+	}
+	int numProcesses, quantumValue;
+	file >> numProcesses >> quantumValue;
+	for (int i = 0; i < numProcesses; ++i) {
+    	int pid, priority, arrivalTime;
+    	file >> pid >> priority >> arrivalTime;
+
+    	vector<string> bursts;
+    	string burst;
+    	while (file >> burst && burst != "-1") {
+        	bursts.push_back(burst);
+    	}
+    	processes.emplace_back(pid, priority, arrivalTime, bursts);
+	}
+	file.close();
+	return processes;
+}
+
+int main() {
+	vector<Process> processes = readInputFile("input.dat");
+	sort(processes.begin(), processes.end(), [](const Process& p1, const Process& p2) {
+    	return p1.arrivalTime < p2.arrivalTime;
+	});
+	fcfsScheduling(processes);
+	return 0;
+}
